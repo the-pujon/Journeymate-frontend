6c7b8937ddd38ce5c1485a8b0ee488ec9c7069be
Pujon Das Auvi <auvipujondas@gmail.com>
2024-10-13 00:49:46 +0600

    people profile page added with full api implementation

--- src/app/profile/[peopleId]/page.tsx
+++ src/app/profile/[peopleId]/page.tsx
@@ -1,11 +1,24 @@
+"use client"
+
+import Loading from '@/components/shared/Loading';
+import Profile from '@/components/shared/Profile';
+import { useGetUserByIdQuery } from '@/redux/features/user/userApi';
 import React from 'react';
 
 const PeopleDetailsPage = ({ params }: { params: { peopleId: string } }) => {
     const { peopleId } = params;
-    return (
-        <div>
+    const { data: userProfile,isLoading,isError } = useGetUserByIdQuery(peopleId || '');
 
-        </div>
+    if (isLoading) {
+        return <Loading />;
+    }
+
+    if (isError || !userProfile) {
+        return <div>Error loading profile</div>;
+    }
+
+    return (
+        <Profile userProfile={userProfile} />
     );
 };
 
